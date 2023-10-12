87ba7ccc4031cdcf2e6fd3962a571ae6f68adea5
Suphakit P <me@suphakit.net>
2023-10-12 08:55:16 +0700

    chore<about>: fix some styling

--- src/components/pages/Me.tsx
+++ src/components/pages/Me.tsx
@@ -3,16 +3,16 @@ import DetailedItem from "../DetailedItem";
 
 export default function Me() {
   return (
-    <div id="about" className="w-full h-screen">
+    <div id="about" className="w-full md:h-screen h-auto">
       <div className="w-full h-[10vh] flex flex-row items-center justify-start text-left underline">
         <h3 className="text-2xl px-8">Who Am I?</h3>
       </div>
-      <div className="w-full h-[90vh] flex flex-row">
+      <div className="w-full h-auto md:h-[90vh] flex flex-col md:flex-row gap-y-[2rem] md:gap-0">
         <div className="w-full flex flex-row justify-center items-center text-center">
           <img
             src="/Me_Profile.webp"
             alt="profile"
-            className="rounded-[50%] p-4 border-solid border-[5px] border-ctp-sapphire"
+            className="rounded-[50%] p-4 border-solid border-[5px] border-ctp-sapphire md:w-[500px] w-[250px]"
           />
         </div>
         <div className="w-full">
