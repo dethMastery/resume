4a54b3f4d73cb4bd1f0b87a812434a06c3e6adf4
Suphakit P <me@suphakit.net>
2023-10-14 05:36:58 +0700

    chore<skill>: change skill id

--- src/components/pages/Skills.tsx
+++ src/components/pages/Skills.tsx
@@ -3,7 +3,7 @@ import SkillBox from "../SkillBox";
 
 export default function Skills() {
   return (
-    <div id="skill" className="w-full min-h-screen">
+    <div id="skills" className="w-full min-h-screen">
       <div className="w-full h-[10vh] flex flex-row items-center justify-start text-left underline">
         <h3 className="text-2xl px-8">Skills</h3>
       </div>
